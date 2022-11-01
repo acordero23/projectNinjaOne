5c9794a2a03bea4d414493ac4a5a3d8ad575fd00
github_username <acordero@technisys.com>
2022-11-01 05:04:28 -0500

    creacion de registros y tablas.

--- infrastructure/adapter-persistence-spring-data-jpa/target/classes/data.sql
+++ infrastructure/adapter-persistence-spring-data-jpa/target/classes/data.sql
@@ -2,6 +2,9 @@ INSERT INTO devices (id, system_name, type, cost) VALUES (1, 'Windows', 'Windows
 INSERT INTO devices (id, system_name, type, cost) VALUES (2, 'Windows', 'Windows Server', 4.00);
 INSERT INTO devices (id, system_name, type, cost) VALUES (3, 'Mac', 'Mac', 4.00);
 
-INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (1, 'Each Device', 1, 4.00);
-INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (2, 'Antivirus windows', 1, 5.00);
-INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (3, 'Antivirus mac', 3, 7.00);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (1, 'Each Device', 1, null);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (2, 'Antivirus Windows', 1, 5.00);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (3, 'Antivirus Mac', 3, 7.00);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (4, 'Backup', 1, 3.00);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (5, 'PSA', 1, 2.00);
+INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (6, 'Screen Share', 1, 1.00);
--- infrastructure/adapter-persistence-spring-data-jpa/target/classes/schema.sql
+++ infrastructure/adapter-persistence-spring-data-jpa/target/classes/schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS devices;
-
 CREATE TABLE devices (
     id INT PRIMARY KEY auto_increment,
     system_name VARCHAR(150) NOT NULL,
@@ -8,10 +7,24 @@ CREATE TABLE devices (
 );
 
 DROP TABLE IF EXISTS services_cost;
-
 CREATE TABLE services_cost (
     id INT PRIMARY KEY auto_increment,
     service_name VARCHAR(150) NOT NULL,
     device_id INT NOT NULL REFERENCES devices (id),
-    cost DECIMAL(18,2) NOT NULL
+    cost DECIMAL(18,2)
+);
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+    id INT PRIMARY KEY auto_increment,
+    client_mame VARCHAR(150) NOT NULL,
+    client_identification VARCHAR(150) NOT NULL
+);
+
+DROP TABLE IF EXISTS contract;
+CREATE TABLE contract (
+    id INT PRIMARY KEY auto_increment,
+    cliente_id INT NOT NULL REFERENCES clients (id),
+    service_cost_id INT NOT NULL REFERENCES services_cost (id),
+    quantity INT NOT NULL
 );
