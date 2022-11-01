c8bba604580287fe1ad050bafc038ab2e5c76529
github_username <acordero@technisys.com>
2022-11-01 05:18:07 -0500

    modificacion de registros.

--- infrastructure/adapter-persistence-spring-data-jpa/target/classes/data.sql
+++ infrastructure/adapter-persistence-spring-data-jpa/target/classes/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO devices (id, system_name, type, cost) VALUES (1, 'Windows', 'Windows Workstation', 4.00);
-INSERT INTO devices (id, system_name, type, cost) VALUES (2, 'Windows', 'Windows Server', 4.00);
+INSERT INTO devices (id, system_name, type, cost) VALUES (1, 'Windows Workstation', 'Windows Workstation', 4.00);
+INSERT INTO devices (id, system_name, type, cost) VALUES (2, 'Windows Server', 'Windows Server', 4.00);
 INSERT INTO devices (id, system_name, type, cost) VALUES (3, 'Mac', 'Mac', 4.00);
 
 INSERT INTO services_cost (id, service_name, device_id, cost) VALUES (1, 'Each Device', 1, null);
